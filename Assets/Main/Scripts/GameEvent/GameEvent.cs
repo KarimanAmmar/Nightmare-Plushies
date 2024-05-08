06d82459bc083aec3347aca4488c6501d8dc4467
KarimanAmmar <76701636+KarimanAmmar@users.noreply.github.com>
2024-05-08 14:49:48 +0300

    GameEvent added

---
+++ Assets/Main/Scripts/GameEvent/GameEvent.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(menuName = "Event/GameEvent", order = 1)]
+public class GameEvent : ScriptableObject
+{
+    UnityAction gameAction;
+    public UnityAction GameAction { get { return gameAction; } set { gameAction = value; } }
+}
