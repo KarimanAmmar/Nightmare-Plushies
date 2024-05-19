2539a3cd6b78e50bb12a89dec6b95e29826eb55c
beshoycoder <99363520+beshoycoder@users.noreply.github.com>
2024-05-19 12:07:58 +0300

    scripts added

---
+++ Assets/Beshoy/Scripts/upgrade system/UpgradeManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// in this script :
+/// handle how the upgrades are selected for each the player can upgrade 
+/// apply the upgrade by invoking an event that transfer data to a script 
+/// that hold all the players data that can be upgraded(coming soon)
+/// listens to an event to trigger the selection process of the upgrades
+/// after the play chooses the upgrade the manager should remove the upgrade he chose 
+/// or decrease the chance to appear again
+/// there are 2 type of upgrade:
+/// the ones that appear during the level (normal)
+/// and the ones that appear after deafeatinng the boss of the level(higher or rare)
+/// </summary>
+public class UpgradeManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
