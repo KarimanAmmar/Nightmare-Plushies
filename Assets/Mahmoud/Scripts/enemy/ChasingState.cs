08813c4678485cf6eeaba0830a572b402f2ad761
mahmoud abdelrazk <83193905+mahmud195@users.noreply.github.com>
2024-05-19 09:49:37 +0300

    proto type with art and testing on Mobile

--- Assets/Mahmoud/Scripts/enemy/ChasingState.cs
+++ Assets/Mahmoud/Scripts/enemy/ChasingState.cs
@@ -19,11 +19,19 @@ public class ChasingState : IEnemyState
 	public void UpdateState(EnemyController enemy, Vector3 playerPosition)
 	{
 		MoveTowardsPlayer(playerPosition);
+		LookAtPlayer(playerPosition);
 	}
 
 	private void MoveTowardsPlayer(Vector3 playerPosition)
 	{
 		Vector3 moveDirection = (playerPosition - rb.position).normalized;
 		rb.MovePosition(rb.position + moveDirection * moveSpeed * Time.deltaTime);
+	}
+
+	private void LookAtPlayer(Vector3 playerPosition)
+	{
+		Vector3 lookDirection = (playerPosition - rb.position).normalized;
+		Quaternion targetRotation = Quaternion.LookRotation(new Vector3(lookDirection.x, 0f, lookDirection.z));
+		rb.rotation = Quaternion.Lerp(rb.rotation, targetRotation, Time.deltaTime);
 	}
 }
