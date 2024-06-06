9f04a68a6df11d27c4180bf726851627f8d442d3
beshoycoder <99363520+beshoycoder@users.noreply.github.com>
2024-06-06 11:41:24 +0300

    scene manger script updated & fixed bugs in the upgrade system
    i update the scene manager script where in the OpenScene function the manager will start an IEnumerator to will start a Async load scene for the cameplay scene and fill the loading bar until the scene is ready to load ,
    there is a bug found in the upgrade system where the button of the upgrade otions are not cliclkable and it was solved by changing the sort order of the parent canvas of the upgrade panel form 0 to 1

--- Assets/Beshoy/Scripts/SceneManager.cs
+++ Assets/Beshoy/Scripts/SceneManager.cs
@@ -2,13 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Scenemanager : MonoBehaviour
 {
     [SerializeField] private string sceneName;
+    [SerializeField] GameObject loading_screen;
+    [SerializeField] GameObject Menu_screen;
+    [SerializeField] Slider loading_bar;
+    
 
     public void OpenScene()
     {
-        SceneManager.LoadScene(sceneName);
+        Menu_screen.SetActive(false);
+        loading_screen.SetActive(true);
+        StartCoroutine(OpenAsyncScene());
+
     }
+    IEnumerator OpenAsyncScene() 
+    { 
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        while (!operation.isDone)
+        {
+            float value = Mathf.Clamp01(operation.progress / 0.9f);
+            loading_bar.value = value;
+
+            yield return null;
+        }
+        
+    }
+
 }
