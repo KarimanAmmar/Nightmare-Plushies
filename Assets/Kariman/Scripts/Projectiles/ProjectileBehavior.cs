dc3989ecfcdc4d066837feb128640a4d9eda4871
KarimanAmmar <76701636+KarimanAmmar@users.noreply.github.com>
2024-06-15 18:29:01 +0300

    thorns added to arena 2

--- Assets/Kariman/Scripts/Projectiles/ProjectileBehavior.cs
+++ Assets/Kariman/Scripts/Projectiles/ProjectileBehavior.cs
@@ -46,34 +46,5 @@ public class ProjectileBehavior : MonoBehaviour
         yield return waitTime;
         this.gameObject.SetActive(false);
     }
-    //Transform targetPoint;
-    //[SerializeField] int speed;
-    //[SerializeField] int waitTimeNum = 3;
-    //WaitForSeconds waitTime;
-    //private void Start()=> waitTime = new WaitForSeconds(waitTimeNum);
-    //private void Update() => Move();
-    //void Move()
-    //{
-    //    if (targetPoint != null)
-    //    {
-    //        Logging.Log("Target Not Null");
-    //        Vector3 direction = (targetPoint.position - transform.position).normalized;
-    //        transform.Translate(direction * speed * Time.deltaTime, Space.World);
-    //    }
-    //    else
-    //    {
-    //        Logging.Log("Target Null");
-    //    }
-    //}
-    //public void Initialize(Transform target, int moveSpeed)
-    //{
-    //    targetPoint = target;
-    //    speed = moveSpeed;
-    //    StartCoroutine(DeactivateObject());
-    //}
-    //IEnumerator DeactivateObject()
-    //{
-    //    yield return waitTime;
-    //    this.gameObject.SetActive(false);
-    //}
+   
 }
