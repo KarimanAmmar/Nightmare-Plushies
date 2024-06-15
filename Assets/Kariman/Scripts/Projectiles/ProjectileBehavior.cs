89ce745033d9e7202044c15692b9c4796c27ed1c
KarimanAmmar <76701636+KarimanAmmar@users.noreply.github.com>
2024-06-15 17:30:45 +0300

    projectiles bugs fixed

--- Assets/Kariman/Scripts/Projectiles/ProjectileBehavior.cs
+++ Assets/Kariman/Scripts/Projectiles/ProjectileBehavior.cs
@@ -5,33 +5,75 @@ using UnityEngine;
 public class ProjectileBehavior : MonoBehaviour
 {
     Transform targetPoint;
+    Vector3 direction;
     [SerializeField] int speed;
     [SerializeField] int waitTimeNum = 3;
     WaitForSeconds waitTime;
-    private void Start()=> waitTime = new WaitForSeconds(waitTimeNum);
-    private void Update() => Move();
+
+    private void Start()
+    {
+        waitTime = new WaitForSeconds(waitTimeNum);
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
     void Move()
     {
-        if (targetPoint != null)
+        if (direction != Vector3.zero)
         {
-            Logging.Log("Target Not Null");
-            Vector3 direction = (targetPoint.position - transform.position).normalized;
+            Logging.Log("Moving in Direction: " + direction);
             transform.Translate(direction * speed * Time.deltaTime, Space.World);
         }
         else
         {
-            Logging.Log("Target Null");
+            Logging.Log("No Direction Set");
         }
     }
+
     public void Initialize(Transform target, int moveSpeed)
     {
         targetPoint = target;
         speed = moveSpeed;
+        direction = (targetPoint.position - transform.position).normalized;
         StartCoroutine(DeactivateObject());
     }
+
     IEnumerator DeactivateObject()
     {
         yield return waitTime;
         this.gameObject.SetActive(false);
     }
+    //Transform targetPoint;
+    //[SerializeField] int speed;
+    //[SerializeField] int waitTimeNum = 3;
+    //WaitForSeconds waitTime;
+    //private void Start()=> waitTime = new WaitForSeconds(waitTimeNum);
+    //private void Update() => Move();
+    //void Move()
+    //{
+    //    if (targetPoint != null)
+    //    {
+    //        Logging.Log("Target Not Null");
+    //        Vector3 direction = (targetPoint.position - transform.position).normalized;
+    //        transform.Translate(direction * speed * Time.deltaTime, Space.World);
+    //    }
+    //    else
+    //    {
+    //        Logging.Log("Target Null");
+    //    }
+    //}
+    //public void Initialize(Transform target, int moveSpeed)
+    //{
+    //    targetPoint = target;
+    //    speed = moveSpeed;
+    //    StartCoroutine(DeactivateObject());
+    //}
+    //IEnumerator DeactivateObject()
+    //{
+    //    yield return waitTime;
+    //    this.gameObject.SetActive(false);
+    //}
 }
